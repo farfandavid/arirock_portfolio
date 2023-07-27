3ff0658d6c8fa55563dd0647a07920b2b55b9da9
farfandavid <70915094+farfandavid@users.noreply.github.com>
2023-07-27 18:58:02 -0300

    a

--- src/services/products.js
+++ src/services/products.js
@@ -1,13 +1,9 @@
 
-//const DATABASE_ID = "1e0983150b5a422faebd14a13448637b";
 
-/* const notion = new Client({
-  auth: "secret_okhrxHQhuac9nlXzSv6PsthoXG0o5L11zopG8GHT7RW"
-}) */
 
 export async function NotionApi() {
   var myHeaders = new Headers();
-  myHeaders.append("Authorization", "Bearer secret_okhrxHQhuac9nlXzSv6PsthoXG0o5L11zopG8GHT7RW");
+  myHeaders.append("Authorization", "");
   myHeaders.append("Notion-Version", "2022-06-28");
   myHeaders.append("Content-Type", "application/json");
 
